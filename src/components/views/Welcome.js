4ae462322565b9f0e324076bc77f56cc04fa15bd
UpstairsForest <nikita.batalov@uzh.ch>
2022-04-02 20:18:27 +0200

    fix #8 Add basic functionality to welcome page

--- src/components/views/Welcome.js
+++ src/components/views/Welcome.js
@@ -1,7 +1,46 @@
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
+import { api, handleError } from "helpers/api";
+
 export const Welcome = () => {
-    return (
-        <div>
-            Welcome
-        </div>
-    )
+  const history = useHistory();
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    !!localStorage.getItem("token") && !!localStorage.getItem("id")
+  );
+
+  let content;
+
+  async function doLogout() {
+    try {
+      // todo: check REST specification
+      const requestBody = JSON.stringify({ isOnline: false });
+      await api.put("/user/" + localStorage.getItem("id"), requestBody);
+    } catch (e) {
+      alert(`Something went wrong during logout: \n${handleError(e)}`);
+    } finally {
+      localStorage.removeItem("token");
+      localStorage.removeItem("id");
+      setIsAuthenticated(false);
+    }
+  }
+
+  if (isAuthenticated) {
+    content = (
+      <div>
+        <button onClick={() => history.push("/user/calendar")}>
+          my calendar
+        </button>
+        <button onClick={() => doLogout()}> log out</button>
+      </div>
+    );
+  } else {
+    content = (
+      <div>
+        <button onClick={() => history.push("/login")}>sign in</button>
+        <button onClick={() => history.push("/register")}>sign up</button>
+      </div>
+    );
+  }
+
+  return content;
 };

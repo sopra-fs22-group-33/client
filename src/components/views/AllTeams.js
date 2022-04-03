167f7833a52842f613d5de2d04dc1c7d478af0b2
eoeaee <jerome.faessler98@gmail.com>
2022-04-03 15:13:15 +0200

    Added AllTeams page

--- src/components/views/AllTeams.js
+++ src/components/views/AllTeams.js
@@ -1,17 +1,84 @@
-import { useHistory } from "react-router-dom";
-import { doLogout } from "../../helpers/api";
+import {useHistory} from "react-router-dom";
+import {api, doLogout, handleError} from "../../helpers/api";
+import {useState} from "react";
+import User from "../../models/User";
+
+
+const Team = ({team, getTeam}) => (
+
+    <div className="team container">
+        <a className="team name" onClick={() => getTeam(team.id) }>{team.name} </a>
+        <div className="team id">id: {team.id}</div>
+
+        <ul className="team member-list">
+            {team.map(teamMember => (
+                <TeamMember
+                    teamMember={teamMember}
+                />
+
+            ))}
+        </ul>
+
+    </div>
+
+);
+const TeamMember = ({team}) => (
+
+    <div className="team member container">
+        <div className="team member id">id: {team.id}</div>
+    </div>
+
+);
+
+Team.propTypes = {
+    user: PropTypes.object
+};
+
 
 export const AllTeams = () => {
-  const history = useHistory();
-  //change
+    const history = useHistory();
+    const user = localStorage.getUser();
 
-  return (
-    <div>
-      <button onClick={() => history.push("/team/calendar")}>calendar</button>
-      <button onClick={() => history.push("/user")}>me</button>
-      <button onClick={() => doLogout().then(() => history.push("/"))}>
-        logout
-      </button>
-    </div>
-  );
+    //hooks
+    const [teams, setTeams] = useState(null);
+
+    //fetch all teams user is part of from backend
+    const fetchData = async (props) => {
+        try {
+            const response = await api.get(`/users/${user.id}/teams`);
+            setTeams(response.data);
+        } catch (error) {
+            alert(`Something went wrong with fetching the teams the user is part of: \n${handleError(error)}`);
+        }
+    }
+    //execute
+    fetchData();
+
+    let content = <Spinner/>;
+
+    if (teams) {
+        content = (
+            <div className="game">
+                <ul className="game user-list">
+                    {teams.map(team => (
+                        <Team
+                            team={team}
+                        />
+
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <button onClick={() => history.push("/team/calendar")}>calendar</button>
+            <button onClick={() => history.push("/user")}>me</button>
+            <button onClick={() => doLogout().then(() => history.push("/"))}>
+                logout
+            </button>
+            {content}
+        </div>
+    );
 };

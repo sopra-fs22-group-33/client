cd985c19272d5b6255a1311185ea9bdc361bdee2
UpstairsForest <nikita.batalov@uzh.ch>
2022-04-09 23:07:06 +0200

    fix #53 Update rough calendar with rough day class

--- src/components/ui/Calendar.js
+++ src/components/ui/Calendar.js
@@ -1,71 +1,71 @@
-import * as React from 'react';
+import * as React from "react";
 import Box from "@mui/material/Box";
-import {Grid} from "@mui/material";
-import {styled} from "@mui/material/styles";
+import { Grid } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import PropTypes from "prop-types";
 
-const Item = styled(Paper)(({theme}) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
 }));
 
+class Day extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slots: props.slots,
+    };
+  }
+
+  render() {
+    const xs = 12 / 7;
+    const md = false;
+
+    return (
+      <Grid item xs={xs} md={md}>
+        <Item>
+          weekday: {this.props.weekday}
+          slots: {this.props.slots}
+        </Item>
+      </Grid>
+    );
+  }
+}
+
+Day.propTypes = {
+  slots: PropTypes.array,
+};
+
 export class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: props.days,
+    };
+  }
 
-    constructor(props) {
-
-        super(props);
-        this.state = {
-            startingDate: props.startingDate,
-            days: props.days,
-        };
-    }
-
-    render() {
-        const xs = 12 / 7;
-        const md = false;
-
-        return (
-            <div>
-                <div>
-                    {this.state.days}
-                </div>
-                <Box sx={{width: 1}}>
-                    <Box sx={{flexGrow: 1}}>
-                        <Grid container spacing={1}>
-                            <Grid item xs={xs} md={md}>
-                                <Item>day</Item>
-                            </Grid>
-                            <Grid item xs={xs} md={md}>
-                                <Item>day</Item>
-                            </Grid>
-                            <Grid item xs={xs} md={md}>
-                                <Item>day</Item>
-                            </Grid>
-                            <Grid item xs={xs} md={md}>
-                                <Item>day</Item>
-                            </Grid>
-                            <Grid item xs={xs} md={md}>
-                                <Item>day</Item>
-                            </Grid>
-                            <Grid item xs={xs} md={md}>
-                                <Item>day</Item>
-                            </Grid>
-                            <Grid item xs={xs} md={md}>
-                                <Item>day</Item>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Box>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Box sx={{ width: 1 }}>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+              {this.state.days.map((day) => (
+                <Day weekday={day.weekday} slots={day.slots} />
+              ))}
+            </Grid>
+          </Box>
+        </Box>
+      </div>
+    );
+  }
 }
 
 Calendar.propTypes = {
-    startingDate: PropTypes.string,
-    days: PropTypes.array
-}
+  startingDate: PropTypes.string,
+  days: PropTypes.array,
+};
--- src/components/views/TeamCalendar.js
+++ src/components/views/TeamCalendar.js
@@ -1,16 +1,33 @@
 import { useHistory } from "react-router-dom";
-import { doLogout } from "../../helpers/api";
+import { doLogout } from "helpers/api";
+import * as React from "react";
+import { Calendar } from "components/ui/Calendar";
 
 export const TeamCalendar = () => {
   const history = useHistory();
 
+  let validatedCalendar = {
+    startingDate: "someDateFormat",
+    days: [
+      { weekday: "MONDAY", slots: ["range", "range"] },
+      { weekday: "TUESDAY", slots: ["range", "range"] },
+    ],
+  };
+
   return (
     <div>
-      <button onClick={() => history.push("/team/profile")}>profile</button>
-      <button onClick={() => history.push("/user")}>me</button>
-      <button onClick={() => doLogout().then(() => history.push("/"))}>
-        logout
-      </button>
+      <div>
+        button container
+        <button onClick={() => history.push("/team/profile")}>profile</button>
+        <button onClick={() => history.push("/user")}>me</button>
+        <button onClick={() => doLogout().then(() => history.push("/"))}>
+          logout
+        </button>
+      </div>
+      <div>
+        calendar container
+        <Calendar startingDate={validatedCalendar.startingDate} days={validatedCalendar.days} />
+      </div>
     </div>
   );
 };

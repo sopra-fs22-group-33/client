7792cc1902306b7198a81d7a0b5ed69f2f4a156d
UpstairsForest <nikita.batalov@uzh.ch>
2022-04-10 17:09:58 +0200

    close #53 Define calendar validation method signature

--- src/components/views/TeamCalendar.js
+++ src/components/views/TeamCalendar.js
@@ -2,24 +2,24 @@ import { useHistory } from "react-router-dom";
 import { doLogout } from "helpers/api";
 import * as React from "react";
 import { Calendar } from "components/ui/Calendar";
+import {EXAMPLE_VALIDATED_CALENDAR} from "../../fixtures/exampleCalendar";
+
 
 export const TeamCalendar = () => {
   const history = useHistory();
 
-  let validatedCalendar = {
-    startingDate: "someDateFormat",
-    days: [
-      { weekday: "MONDAY", slots: [{from: 0, to: 20}, {from: 0, to: 10}] },
-      { weekday: "TUESDAY", slots: [{from: 0, to: 10}, {from: 0, to: 10}] },
-      { weekday: "TUESDAY", slots: [{from: 0, to: 10}, {from: 0, to: 10}] },
-      { weekday: "TUESDAY", slots: [{from: 0, to: 10}, {from: 0, to: 10}] },
-      { weekday: "TUESDAY", slots: [{from: 0, to: 10}, {from: 0, to: 10}] },
-      { weekday: "TUESDAY", slots: [{from: 0, to: 10}, {from: 0, to: 10}] },
-      { weekday: "TUESDAY", slots: [{from: 0, to: 10}, {from: 0, to: 10}] },
-      { weekday: "TUESDAY", slots: [{from: 0, to: 10}, {from: 0, to: 10}] },
-      { weekday: "TUESDAY", slots: [{from: 0, to: 10}, {from: 0, to: 10}] },
-    ],
-  };
+    /**
+     * validates a calendar, tries to get relevant data on assignedUsers
+     *
+     * @param {object} calendar
+     * @returns {{startingDate: string, days: array}}
+     */
+  function validateCalendar(calendar) {
+      let validatedCalendar = EXAMPLE_VALIDATED_CALENDAR;
+      return validatedCalendar;
+  }
+
+  const validatedCalendar = validateCalendar({});
 
   return (
     <div>

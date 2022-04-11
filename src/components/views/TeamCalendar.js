0b9fbdfc1ea76b1602c75c729c73f42146967ef1
UpstairsForest <nikita.batalov@uzh.ch>
2022-04-11 15:05:35 +0200

    fix #52 Add method to fetch team calendar from server

--- src/components/views/TeamCalendar.js
+++ src/components/views/TeamCalendar.js
@@ -1,14 +1,32 @@
 import { useHistory } from "react-router-dom";
-import { doLogout } from "helpers/api";
+import { api, doLogout, handleError } from "helpers/api";
 import * as React from "react";
 import { Calendar } from "components/ui/Calendar";
-import {validateCalendar} from "helpers/validations";
-import {VALID_TEAM_CALENDAR} from "fixtures/exampleCalendar";
+import { validateCalendar } from "helpers/validations";
+import { VALID_TEAM_CALENDAR } from "fixtures/exampleCalendar";
+import { useEffect, useState } from "react";
 
 export const TeamCalendar = () => {
   const history = useHistory();
+  const [calendar, setCalendar] = useState(VALID_TEAM_CALENDAR);
 
-  const validatedCalendar = validateCalendar(VALID_TEAM_CALENDAR);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await api.get(
+          `/teams/${localStorage.getItem("teamId")}/calendars`
+        );
+
+        setCalendar(response.data);
+      } catch (e) {
+        alert(`Something went wrong during the login: \n${handleError(e)}`);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const validatedCalendar = validateCalendar(calendar);
 
   return (
     <div>

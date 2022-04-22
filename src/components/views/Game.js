6fb4748db1f5e7784c2a25fdcbc17f9c692832bf
Thalia Lynn Fox <thalia.fox@bluewin.ch>
2022-04-22 19:02:20 +0200

    started on game

---
+++ src/components/views/Game.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { api, handleError } from "helpers/api";
+import { useHistory } from "react-router-dom";
+import { Button } from "components/ui/Button";
+import "styles/views/Auth.scss";
+import BaseContainer from "components/ui/BaseContainer";
+import { FormField } from "components/ui/FormField";
+
+const Game = (props) => {
+
+    const updateGame = () => {
+
+        // GET request
+
+    }
+
+    const makeMove = () => {
+
+        // PUT request
+    }
+
+
+    return (
+        <BaseContainer>
+
+        </BaseContainer>
+    );
+};
+
+// do export here if we create multiple const

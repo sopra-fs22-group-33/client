8e9d63c67787853c701fa1e5cdab1af672b42c31
UpstairsForest <nikita.batalov@uzh.ch>
2022-04-23 08:24:39 +0200

    Update example calendar to be more realistic

--- src/fixtures/exampleCalendar.js
+++ src/fixtures/exampleCalendar.js
@@ -4,86 +4,60 @@ export const VALID_TEAM_CALENDAR = {
     {
       weekday: "MONDAY",
       slots: [
-        { from: 0, to: 20 },
-        { from: 0, to: 10 },
+        { from: 0, to: 5 },
+        { from: 20, to: 24 },
       ],
     },
     {
       weekday: "TUESDAY",
       slots: [
         { from: 0, to: 10 },
-        { from: 0, to: 10 },
+        { from: 10, to: 18 },
       ],
     },
     {
-      weekday: "TUESDAY",
+      weekday: "WEDNESDAY",
+      slots: [{ from: 0, to: 10 }],
+    },
+    {
+      weekday: "THURSDAY",
+      slots: [{ from: 0, to: 10 }],
+    },
+    {
+      weekday: "FRIDAY",
+      slots: [{ from: 0, to: 10 }],
+    },
+    {
+      weekday: "SATURDAY",
       slots: [
         { from: 0, to: 10 },
         { from: 0, to: 10 },
       ],
     },
     {
-      weekday: "TUESDAY",
+      weekday: "SUNDAY",
       slots: [
         { from: 0, to: 10 },
         { from: 0, to: 10 },
       ],
     },
     {
-      weekday: "TUESDAY",
+      weekday: "MONDAY",
       slots: [
-        { from: 0, to: 10 },
-        { from: 0, to: 10 },
+        { from: 0, to: 5 },
+        { from: 20, to: 24 },
       ],
     },
     {
       weekday: "TUESDAY",
       slots: [
         { from: 0, to: 10 },
-        { from: 0, to: 10 },
+        { from: 10, to: 18 },
       ],
     },
     {
-      weekday: "TUESDAY",
-      slots: [
-        { from: 0, to: 10 },
-        { from: 0, to: 10 },
-      ],
-    },
-    {
-      weekday: "TUESDAY",
-      slots: [
-        { from: 0, to: 10 },
-        { from: 0, to: 10 },
-      ],
-    },
-    {
-      weekday: "TUESDAY",
-      slots: [
-        { from: 0, to: 10 },
-        { from: 0, to: 10 },
-      ],
-    },
-    {
-      weekday: "TUESDAY",
-      slots: [
-        { from: 0, to: 10 },
-        { from: 0, to: 10 },
-      ],
-    },
-    {
-      weekday: "TUESDAY",
-      slots: [
-        { from: 0, to: 10 },
-        { from: 0, to: 10 },
-      ],
-    },
-    {
-      weekday: "TUESDAY",
-      slots: [
-        { from: 0, to: 10 },
-        { from: 0, to: 10 },
-      ],
+      weekday: "WEDNESDAY",
+      slots: [{ from: 0, to: 10 }],
     },
   ],
 };

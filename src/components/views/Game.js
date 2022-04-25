8a9981d26e9859d7111f5440ad4db38245de3c13
Thalia Lynn Fox <thalia.fox@bluewin.ch>
2022-04-25 02:37:16 +0200

    displaying rudimentary snake works now

--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -4,40 +4,69 @@ import { useHistory } from "react-router-dom";
 import { Button } from "components/ui/Button";
 import "styles/views/Auth.scss";
 import BaseContainer from "components/ui/BaseContainer";
-import { FormField } from "components/ui/FormField";
 
 const Snake = (props) => {
 
     const Snake = [100,100];
-    const snakeSize = 1;
 
     const step = 50;
 
+    return (
+        <div
+        style={{
+            position: "absolute",
+            top: 250,
+            left: 250,
+            height: 50,
+            width: 50,
+            background: "green",
+        }}></div>
+    )
 
 }
 
+const Gameboard = (props) => {
+
+    return (
+        <div
+        style={{
+            position: "absolute",
+            top: 200,
+            left: 200,
+            height: 500,
+            width: 500,
+            background: "black",
+        }}>
+            <Snake></Snake>
+    </div>
+    )
+}
+
+
 export const Game = (props) => {
 
-    const getUpdatedGame = () => {
+    const currentGame = useState(null);
 
-        // GET request
+    const getUpdatedGame = async () => {
+
+        const currentGame= (await api.get('/game')).data;
+        console.log("yes");
 
     }
+
+    // timer works (8fps), api request doesn't, http 500
+    // setInterval(getUpdatedGame, 125);
 
     const makeMove = () => {
 
         // PUT request
     }
 
+
+
     return (
-        <div
-        style={{
-        height: 500,
-        width: 500,
-        background: "black",
-        }}>
-            <div></div>
-        </div>
+        <Gameboard>
+        </Gameboard>
     )
 
 };

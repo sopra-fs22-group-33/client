bd515035480327284797a1c16da86e0a0f66e8af
UpstairsForest <nikita.batalov@uzh.ch>
2022-04-27 16:52:14 +0200

    Move all teams to user router

--- src/components/routing/routers/TeamRouter.js
+++ src/components/routing/routers/TeamRouter.js
@@ -2,16 +2,12 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import PropTypes from "prop-types";
 import {TeamCalendar} from "components/views/Team/TeamCalendar";
 import {TeamProfile} from "components/views/Team/TeamProfile";
-import {AllTeams} from "../../views/Team/AllTeams";
 import {TeamEdit} from "../../views/Team/TeamEdit";
 
 export const TeamRouter = (props) => {
   return (
     <div>
       <Switch>
-        <Route exact path={`${props.base}/all`}>
-          <AllTeams />
-        </Route>
         <Route exact path={`${props.base}/calendar`}>
           <TeamCalendar />
         </Route>
--- src/components/routing/routers/UserRouter.js
+++ src/components/routing/routers/UserRouter.js
@@ -2,6 +2,7 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import PropTypes from "prop-types";
 import { UserCalendar } from "components/views/User/UserCalendar";
 import { UserProfile } from "components/views/User/UserProfile";
+import { AllTeams } from "../../views/Team/AllTeams";
 
 export const UserRouter = (props) => {
   return (
@@ -10,11 +11,14 @@ export const UserRouter = (props) => {
         <Route exact path={`${props.base}/calendar`}>
           <UserCalendar />
         </Route>
-        <Route exact path={`${props.base}`}>
-          <Redirect to={`${props.base}/calendar`} />
-        </Route>
         <Route exact path={`${props.base}/profile`}>
           <UserProfile />
+        </Route>
+        <Route exact path={`${props.base}/teams`}>
+          <AllTeams />
+        </Route>
+        <Route exact path={`${props.base}`}>
+          <Redirect to={`${props.base}/calendar`} />
         </Route>
       </Switch>
     </div>

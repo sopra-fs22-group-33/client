8819b6e27f471c8197b080537806c836b68403f3
eoeaee <jerome.faessler98@gmail.com>
2022-04-30 15:18:22 +0200

    Updates to theme and styling of components

--- src/components/views/Register.js
+++ src/components/views/Register.js
@@ -1,60 +1,63 @@
 import {api, getToken, handleError} from "helpers/api";
-import { useHistory } from "react-router-dom";
-import { useState } from "react";
+import {useHistory} from "react-router-dom";
+import {useState} from "react";
 import User from "models/User";
 import BaseContainer from "components/ui/BaseContainer";
-import { FormField } from "components/ui/FormField";
-import { Button } from "components/ui/Button";
+import {FormField} from "components/ui/FormField";
+import {Button} from "components/ui/Button";
 
 export const Register = (props) => {
-  const history = useHistory();
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
+    const history = useHistory();
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
 
-  const doRegister = async (props) => {
-    try {
-      const requestBody = JSON.stringify({ email, password });
-      const response = await api.post("/users", requestBody);
+    const doRegister = async (props) => {
+        try {
+            const requestBody = JSON.stringify({email, password});
+            const response = await api.post("/users", requestBody);
 
-      const user = new User(response.data);
-      user.token = getToken(response)
-      localStorage.setItem("token", user.token);
-      localStorage.setItem("id", user.id);
+            const user = new User(response.data);
+            user.token = getToken(response)
+            localStorage.setItem("token", user.token);
+            localStorage.setItem("id", user.id);
 
-      history.push("/user");
-    } catch (e) {
-      alert(`Something went wrong during registration: \n${handleError(e)}`);
-    }
-  };
+            history.push("/user");
+        } catch (e) {
+            alert(`Something went wrong during registration: \n${handleError(e)}`);
+        }
+    };
 
-  return (
-    <BaseContainer>
-      <div className="auth container">
-        <div className="auth form">
-          <FormField
-            label="Email"
-            value={email}
-            onChange={(n) => setEmail(n)}
-          />
-          <FormField
-            label="Password"
-            value={password}
-            onChange={(un) => setPassword(un)}
-          />
-          <div className="auth button-container">
-            <Button
-              disabled={!email || !password}
-              width="100%"
-              onClick={() => doRegister()}
-            >
-              Register
-            </Button>
-          </div>
-          <button onClick={() => history.push("/login")}>
-            Existing user? Sign in instead
-          </button>
-        </div>
-      </div>
-    </BaseContainer>
-  );
+    return (
+        <BaseContainer>
+            <div className="auth container">
+                <div className="auth form">
+                    <FormField
+                        label="Email"
+                        value={email}
+                        onChange={(n) => setEmail(n)}
+                    />
+                    <FormField
+                        label="Password"
+                        value={password}
+                        onChange={(un) => setPassword(un)}
+                    />
+                    <div className="auth button-container">
+                        <Button
+                            disabled={!email || !password}
+                            onClick={() => doRegister()}
+                        >
+                            Register
+                        </Button>
+                    </div>
+                    <div className="auth button-container">
+                        <Button
+                            onClick={() => history.push("/login")}
+                        >
+                            Sign in instead?
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </BaseContainer>
+    );
 };

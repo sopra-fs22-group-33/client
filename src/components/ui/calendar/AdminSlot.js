8185f94d97f238e60561e7f58e5b4178e126dece
UpstairsForest <nikita.batalov@uzh.ch>
2022-05-05 15:30:26 +0200

    Integrate generic slot and slider into slot for admin calendar

--- src/components/ui/calendar/AdminSlot.js
+++ src/components/ui/calendar/AdminSlot.js
@@ -1,138 +1,79 @@
-import * as React from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { SLOT_SCALING } from "./config";
-import Box from "@mui/material/Box";
-import CalendarGlobal from "./CalendarGlobal";
-import CalendarEventDispatcher from "./CalendarEventDispatcher";
-import { FormField } from "../FormField";
+import { Slot } from "./Slot";
+import { SlotSlider } from "./SlotSlider";
+import { MAX_REQUIREMENT, MIN_REQUIREMENT } from "./config";
 
 export class AdminSlot extends React.Component {
   constructor(props) {
     super(props);
-    this.ref = undefined;
-    this.id = props.slot.id;
-    this.hasUpdated = false;
-    this.startedUpdating = false;
-    this.updatingTop = undefined;
-
-    // reference to object in parent
-    this.slot = props.slot;
 
     this.state = {
       timeFrom: props.timeFrom,
       timeTo: props.timeTo,
-      requirement: props.requirement,
-      schedules: props.schedules,
+      requirement: props.requirement ? props.requirement : 1,
+
+      isSelected: false,
     };
   }
 
-  isSelected() {
-    return this.id === CalendarGlobal.getSelectedSlot();
+  handleMouseDown(ev) {
+    // set to dragged
+    // notify calendar
   }
 
-  deselect() {
-    CalendarGlobal.setSelectedSlot(this.isSelected() ? null : this.id);
-    CalendarEventDispatcher.dispatch("onSlotSelected");
+  handleSlotClick(ev) {
+    ev.stopPropagation();
+    this.setState({ isSelected: !this.state.isSelected });
+
+    console.log(this.state.requirement);
   }
 
-  updateRequirement(value) {
-    this.slot.requirement = value;
+  handleSliderChange(ev, value) {
     this.setState({ requirement: value });
   }
 
-  calcHeight() {
-    return SLOT_SCALING * (this.state.timeTo - this.state.timeFrom);
-  }
-
-  calcTop() {
-    return SLOT_SCALING * this.state.timeFrom;
-  }
-
-  onClick(ev) {
+  handleSliderClick(ev) {
+    // prevent deselection of the slot
     ev.stopPropagation();
-    if (!this.hasUpdated) {
-      this.deselect();
-    }
-    this.startedUpdating = false;
-  }
-
-  onMouseDown(ev) {
-    ev.stopPropagation();
-    this.startedUpdating = this.isSelected();
-
-    if (this.startedUpdating) {
-      const rect = this.ref.getBoundingClientRect();
-      this.updatingTop = ev.clientY - (rect.y + rect.height / 2) < 0;
-    }
-  }
-
-  onMouseUp(ev) {
-    ev.stopPropagation();
-    this.hasUpdated = this.startedUpdating;
-    this.startedUpdating = false;
-    if (this.hasUpdated) {
-      if (this.updatingTop) {
-        const newTimeFrom = Math.floor(this.state.timeFrom);
-        this.slot.timeFrom = newTimeFrom;
-        this.setState({ timeFrom: newTimeFrom });
-      } else {
-        const newTimeTo = Math.floor(this.state.timeTo);
-        this.slot.timeTo = newTimeTo;
-        this.setState({ timeTo: newTimeTo });
-      }
-    }
-  }
-
-  onMouseMove(ev) {
-    if (this.isSelected() && this.startedUpdating) {
-      ev.stopPropagation();
-
-      if (this.updatingTop) {
-        this.setState({
-          timeFrom: this.state.timeFrom + ev.movementY / SLOT_SCALING,
-        });
-      } else {
-        this.setState({
-          timeTo: this.state.timeTo + ev.movementY / SLOT_SCALING,
-        });
-      }
-    }
   }
 
   render() {
     return (
-      <Box
-        ref={(el) => {
-          if (!el) return;
-          this.ref = el;
-        }}
-        sx={{
-          width: 3 / 4,
-        }}
-        style={{
-          position: "absolute",
-          height: this.calcHeight(),
-          top: this.calcTop(),
-          background: this.isSelected() ? "orange" : "gray",
-          opacity: 0.5,
-        }}
-        onClick={(ev) => this.onClick(ev)}
-        onMouseDown={(ev) => this.onMouseDown(ev)}
-        onMouseMove={(ev) => this.onMouseMove(ev)}
-        onMouseUp={(ev) => this.onMouseUp(ev)}
+      <Slot
+        timeFrom={this.props.timeFrom}
+        timeTo={this.props.timeTo}
+        style={{ background: "gray" }}
+        onClick={(ev) => this.handleSlotClick(ev)}
+        onMouseDown={(ev) => this.handleMouseDown(ev)}
+        onMouseMove={(ev) => this.handleMouseMove(ev)}
+        onMouseUp={(ev) => this.handleMouseUp(ev)}
       >
-        required people:{this.state.requirement}
-        <FormField
-          onClick={() => this.deselect.call(this)}
-          value={this.state.requirement}
-          onChange={(value) => this.updateRequirement(value)}
-        />
-      </Box>
+        {this.state.isSelected ? (
+          <SlotSlider
+            style={{
+              position: "relative",
+              top: -20 /* easier than trying to find height of slot */,
+            }}
+            onClick={(ev) => this.handleSliderClick(ev)}
+            onChange={(ev, value) => this.handleSliderChange(ev, value)}
+            value={this.state.requirement}
+            valueLabelDisplay={"auto"}
+            step={1}
+            marks
+            min={MIN_REQUIREMENT}
+            max={MAX_REQUIREMENT}
+          />
+        ) : null}
+      </Slot>
     );
   }
 }
 
 AdminSlot.propTypes = {
-    timeFrom: PropTypes.number,
-    timeTo: PropTypes.number,
+  id: PropTypes.number.isRequired,
+  slot: PropTypes.object,
+  timeFrom: PropTypes.number.isRequired,
+  timeTo: PropTypes.number.isRequired,
+  requirement: PropTypes.number,
 };
--- src/components/ui/calendar/config.js
+++ src/components/ui/calendar/config.js
@@ -1,3 +1,7 @@
 export const DAY_SPACING = 2;
 export const DAY_HEIGHT = 800;
 export const SLOT_SCALING = DAY_HEIGHT / 24;
+
+// min and max number of people per slot
+export const MIN_REQUIREMENT = 1;
+export const MAX_REQUIREMENT = 10;

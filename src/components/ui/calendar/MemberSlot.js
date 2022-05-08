d51d3fc85b6b976bdbfa6487d158159b09695961
UpstairsForest <nikita.batalov@uzh.ch>
2022-05-08 19:43:32 +0200

    Rough interface for setting special preferences

--- src/components/ui/calendar/MemberSlot.js
+++ src/components/ui/calendar/MemberSlot.js
@@ -1,6 +1,42 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 import { Slot } from "./Slot";
+import { MAX_SPECIAL, MIN_SPECIAL } from "./config";
+import { SlotSlider } from "./SlotSlider";
+
+/**
+ * Convert frontend to backend representation
+ */
+function frontToBackSpecial(value) {
+  switch (value) {
+    case -1:
+      return 0;
+    case 0:
+      return -1;
+    case 1:
+      return 1;
+    default:
+      console.log("unhandled value", value);
+      return value;
+  }
+}
+
+/**
+ * Convert backend to frontend representation
+ */
+function backToFrontSpecial(value) {
+  switch (value) {
+    case 0:
+      return -1;
+    case -1:
+      return 0;
+    case 1:
+      return 1;
+    default:
+      console.log("unhandled value", value);
+      return value;
+  }
+}
 
 export class MemberSlot extends React.Component {
   constructor(props) {
@@ -12,42 +48,76 @@ export class MemberSlot extends React.Component {
     this.slot = props.slot;
 
     this.state = {
-      assigned: this.isAssigned(),
+      isSelected: false,
+      mySchedule: this.getSchedule(),
     };
   }
 
   handleClick(ev) {
-    const userId = parseInt(sessionStorage.getItem("id"));
-    if (!this.state.assigned) {
-      this.slot.schedules.push({ special: userId, user: { id: userId } });
-    } else {
-      this.slot.schedules = this.slot.schedules.filter(
-        (o) => o.special !== userId
-      );
-    }
-    this.setState({ assigned: !this.state.assigned });
-    console.log(this.slot.schedules);
+    this.setState({ isSelected: !this.state.isSelected });
   }
 
-  isAssigned() {
-    for (const schedule of this.slot.schedules) {
-      if (schedule.special === parseInt(sessionStorage.getItem("id"))) {
-        return true;
+  handleSliderChange(ev, value) {
+    const schedule = this.state.mySchedule;
+    schedule.special = frontToBackSpecial(value);
+    this.setState({ mySchedule: schedule });
+  }
+
+  getSchedule() {
+    const userId = parseInt(sessionStorage.getItem("id"));
+    for (let schedule of this.slot.schedules) {
+      if (schedule.user.id === userId) {
+        return schedule;
       }
     }
-    return false;
+    // no schedule found
+    const newSchedule = {
+      special: frontToBackSpecial(0),
+      user: { id: userId },
+    };
+    this.slot.schedules.push(newSchedule);
+    return newSchedule;
+  }
+
+  getColor() {
+    const special = backToFrontSpecial(this.state.mySchedule.special);
+    switch (special) {
+      // disliked
+      case -1:
+        return "rgba(196, 128, 18, 0.5)";
+      case 0:
+        return null;
+      case 1:
+        return "rgba(50, 0, 255, 0.5)";
+    }
   }
 
   render() {
     return (
       <Slot
         sx={this.props.sx}
-        style={{ background: this.state.assigned ? "red" : null }}
+        style={{ background: this.state.mySchedule ? this.getColor() : null }}
         timeFrom={this.props.timeFrom}
         timeTo={this.props.timeTo}
         onClick={(ev) => this.handleClick(ev)}
       >
+        {this.state.isSelected ? (
+          <SlotSlider
+            onChange={(ev, value) => this.handleSliderChange(ev, value)}
+            value={backToFrontSpecial(this.state.mySchedule.special)}
+            valueLabelDisplay={"auto"}
+            step={1}
+            marks
+            min={MIN_SPECIAL}
+            max={MAX_SPECIAL}
+          />
+        ) : null}
         <div>req: {this.props.requirement}</div>
+        {this.state.mySchedule ? (
+          <div>
+            my special: {backToFrontSpecial(this.state.mySchedule.special)}
+          </div>
+        ) : null}
       </Slot>
     );
   }
--- src/components/ui/calendar/config.js
+++ src/components/ui/calendar/config.js
@@ -6,3 +6,7 @@ export const SLOT_REL_WIDTH = 3/4;
 // min and max number of people per slot
 export const MIN_REQUIREMENT = 1;
 export const MAX_REQUIREMENT = 10;
+
+// min and max special preference in frontend representation
+export const MIN_SPECIAL = -1;
+export const MAX_SPECIAL = 1;

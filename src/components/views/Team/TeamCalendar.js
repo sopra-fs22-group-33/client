6313c2f7b29fa9f22849e4cf9ed453b0c4261460
UpstairsForest <nikita.batalov@uzh.ch>
2022-05-09 02:48:40 +0200

    Prettier

--- src/components/views/Team/TeamCalendar.js
+++ src/components/views/Team/TeamCalendar.js
@@ -1,82 +1,84 @@
-import {useHistory} from "react-router-dom";
-import {api, doLogout, handleError} from "helpers/api";
+import { useHistory } from "react-router-dom";
+import { api, doLogout, handleError } from "helpers/api";
 import * as React from "react";
-import {validateCalendar} from "helpers/validations";
-import {useEffect, useState} from "react";
-import {SpecialCalendar} from "../../ui/calendar/special/SpecialCalendar";
-import {Button} from "../../ui/Button";
+import { validateCalendar } from "helpers/validations";
+import { useEffect, useState } from "react";
+import { SpecialCalendar } from "../../ui/calendar/special/SpecialCalendar";
+import { Button } from "../../ui/Button";
 import BaseContainer from "../../ui/BaseContainer";
 
 export const TeamCalendar = () => {
-    const history = useHistory();
-    const [calendar, setCalendar] = useState(null);
+  const history = useHistory();
+  const [calendar, setCalendar] = useState(null);
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await api.get(
-                    `/teams/${sessionStorage.getItem("teamId")}/calendars`
-                );
-                console.log(response.data);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await api.get(
+          `/teams/${sessionStorage.getItem("teamId")}/calendars`
+        );
+        console.log(response.data);
 
-                setCalendar(validateCalendar(response.data));
-            } catch (e) {
-                alert(
-                    `Something went wrong during fetching the calendar: \n${handleError(
-                        e
-                    )}`
-                );
-            }
-        }
-
-        fetchData();
-    }, []);
-
-    async function doSave() {
-        try {
-            const requestBody = JSON.stringify({
-                days: calendar.days,
-                startingDate: calendar.startingDate,
-            });
-            await api.put(
-                `/teams/${sessionStorage.getItem("teamId")}/calendars`,
-                requestBody
-            );
-
-            alert("Saved successfully");
-        } catch (error) {
-            alert(
-                `Something went wrong during saving the calendar: \n${handleError(
-                    error
-                )}`
-            );
-        }
+        setCalendar(validateCalendar(response.data));
+      } catch (e) {
+        alert(
+          `Something went wrong during fetching the calendar: \n${handleError(
+            e
+          )}`
+        );
+      }
     }
 
-    if (!calendar) {
-        return <div>fetching calendar</div>;
+    fetchData();
+  }, []);
+
+  async function doSave() {
+    try {
+      const requestBody = JSON.stringify({
+        days: calendar.days,
+        startingDate: calendar.startingDate,
+      });
+      await api.put(
+        `/teams/${sessionStorage.getItem("teamId")}/calendars`,
+        requestBody
+      );
+
+      alert("Saved successfully");
+    } catch (error) {
+      alert(
+        `Something went wrong during saving the calendar: \n${handleError(
+          error
+        )}`
+      );
     }
+  }
 
-    return (
+  if (!calendar) {
+    return <div>fetching calendar</div>;
+  }
 
-        <div>
-            <BaseContainer>
-                <div className="navigation-button-container container">
-                    <div className="navigation-button-container title">
-                        <h1>Team Calendar</h1>
-                    </div>
-                    <div className="navigation-button-container button">
-                        <Button onClick={() => history.push("/team/calendar/edit")}>Edit</Button>
-                        <Button onClick={() => doSave()}>Save</Button>
-                        <Button onClick={() => history.push("/team/profile")}>Team Profile</Button>
-                    </div>
-                </div>
-                <SpecialCalendar
-                    startingDate={calendar.startingDate}
-                    days={calendar.days}
-                />
-            </BaseContainer>
+  return (
+    <div>
+      <BaseContainer>
+        <div className="navigation-button-container container">
+          <div className="navigation-button-container title">
+            <h1>Team Calendar</h1>
+          </div>
+          <div className="navigation-button-container button">
+            <Button onClick={() => history.push("/team/calendar/edit")}>
+              Edit
+            </Button>
+            <Button onClick={() => doSave()}>Save</Button>
+            <Button onClick={() => history.push("/team/profile")}>
+              Team Profile
+            </Button>
+          </div>
         </div>
-
-    );
+        <SpecialCalendar
+          startingDate={calendar.startingDate}
+          days={calendar.days}
+        />
+      </BaseContainer>
+    </div>
+  );
 };

eb68c9443d07a45e9f84d55ed7b0b66d11d7e32f
eoeaee <jerome.faessler98@gmail.com>
2022-05-11 09:25:44 +0200

    Fixed Team edit and prefilled fields with existing values

--- src/components/views/Team/TeamProfileEdit.js
+++ src/components/views/Team/TeamProfileEdit.js
@@ -4,46 +4,53 @@ import { TeamMember } from "../User/AllTeams";
 import { useHistory } from "react-router-dom";
 import BaseContainer from "../../ui/BaseContainer";
 import {Button} from "../../ui/Button";
+import {Spinner} from "../../ui/Spinner";
+import {FormField} from "../../ui/FormField";
 
 export const TeamProfileEdit = () => {
+  // use react-router-dom's hook to access the history
   const history = useHistory();
 
-  const [users, setUsers] = useState(null);
+  const [name, setName] = useState(null);
 
-  async function doSave() {
-    console.log("save");
-  }
+  // define a state variable (using the state hook).
+  // if this variable changes, the component will re-render, but the variable will
+  // keep its value throughout render cycles.
+  // a component can have as many state variables as you like.
+  // more information can be found under https://reactjs.org/docs/hooks-state.html
 
-  useEffect(() => {
-    async function fetchTeamUsers() {
-      try {
-        const response = await api.get(
-          `/teams/${sessionStorage.getItem("teamId")}/users`,
-          {
-            headers: { token: sessionStorage.getItem("token") },
-          }
-        );
-        console.log(response.data);
-        setUsers(response.data);
-      } catch (e) {
-        alert(
-          `Something went wrong while fetching all users:\n${handleError(e)}`
-        );
-      }
-    }
+  const doSaveEditProfile = async () => {
+    const requestBody = JSON.stringify({ name });
+    const id = sessionStorage.getItem("teamId")
+    await api.put(`/teams/${id}`, requestBody, {
+      headers: { token: sessionStorage.getItem("token") },
+    });
 
-    fetchTeamUsers();
-  }, []);
+    // Route to right place here
+    history.goBack();
+  };
 
-  let content = <div>fetching users</div>;
+  let content = <Spinner />;
 
-  if (users) {
-    content = (
-        <div className="team container">
+  content = (
+      <div className="auth container">
+        <div className="auth form">
+          <FormField
+              label="Name"
+              value={name}
+              onChange={(un) => setName(un)}
+          />
 
+          <div className="button-container">
+            <Button disabled={!name} onClick={() => doSaveEditProfile()}>
+              Save
+            </Button>
+            <Button onClick={() => history.goBack()}>Cancel</Button>
+
+          </div>
         </div>
-    );
-  }
+      </div>
+  );
 
   return (
       <BaseContainer>
@@ -52,12 +59,11 @@ export const TeamProfileEdit = () => {
             <h1>Edit Team Profile</h1>
           </div>
           <div className="navigation-button-container button">
-            <Button onClick={doSave}>Save</Button>
-            <Button onClick={() => history.push("/team/profile")}>Cancel</Button>
           </div>
         </div>
         {content}
       </BaseContainer>
-
   );
 };
+
+export default TeamProfileEdit;

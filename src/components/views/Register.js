c55e4bc9b86af70eb477268649be3152d59dd434
UpstairsForest <nikita.batalov@uzh.ch>
2022-05-14 13:45:40 +0200

    Add user preferences calendar creation upon register

--- src/components/views/Register.js
+++ src/components/views/Register.js
@@ -1,70 +1,76 @@
-import {api, getToken, handleError} from "helpers/api";
-import {useHistory} from "react-router-dom";
-import {useState} from "react";
+import { api, getToken, handleError } from "helpers/api";
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
 import User from "models/User";
 import BaseContainer from "components/ui/BaseContainer";
-import {FormField} from "components/ui/FormField";
-import {Button} from "components/ui/Button";
+import { FormField } from "components/ui/FormField";
+import { Button } from "components/ui/Button";
+import { TEMPLATE_USER_CALENDAR } from "../../fixtures/templateUserCalendar";
 
 export const Register = (props) => {
-    const history = useHistory();
-    const [email, setEmail] = useState(null);
-    const [password, setPassword] = useState(null);
-    const [username, setUsername] = useState(null);
+  const history = useHistory();
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [username, setUsername] = useState(null);
 
+  const doRegister = async (props) => {
+    try {
+      const userRequestBody = JSON.stringify({ email, password, username });
+      const userResponse = await api.post("/users", userRequestBody);
 
-    const doRegister = async (props) => {
-        try {
-            const requestBody = JSON.stringify({email, password, username});
-            const response = await api.post("/users", requestBody);
+      const user = new User(userResponse.data);
+      user.token = getToken(userResponse);
 
-            const user = new User(response.data);
-            user.token = getToken(response)
-            sessionStorage.setItem("token", user.token);
-            sessionStorage.setItem("id", user.id);
+      const preferenceRequestBody = JSON.stringify(TEMPLATE_USER_CALENDAR);
+      await api.post(
+        `/users/${user.id}/preferences`,
+        preferenceRequestBody,
+        { headers: { token: user.token } }
+      );
 
-            history.push("/user");
-        } catch (e) {
-            alert(`Something went wrong during registration: \n${handleError(e)}`);
-        }
-    };
+      sessionStorage.setItem("token", user.token);
+      sessionStorage.setItem("id", user.id);
 
-    return (
-        <BaseContainer>
-            <div className="auth container">
-                <div className="auth form">
-                    <FormField
-                        label="Email"
-                        value={email}
-                        onChange={(n) => setEmail(n)}
-                    />
-                    <FormField
-                        label="Username"
-                        value={username}
-                        onChange={(uun) => setUsername(uun)}
-                    />
-                    <FormField
-                        label="Password"
-                        value={password}
-                        onChange={(un) => setPassword(un)}
-                    />
-                    <div className="auth button-container">
-                        <Button
-                            disabled={!email || !password || !username}
-                            onClick={() => doRegister()}
-                        >
-                            Register
-                        </Button>
-                    </div>
-                    <div className="auth button-container">
-                        <Button
-                            onClick={() => history.push("/login")}
-                        >
-                            Sign in instead?
-                        </Button>
-                    </div>
-                </div>
-            </div>
-        </BaseContainer>
-    );
+      history.push("/user");
+    } catch (e) {
+      alert(`Something went wrong during registration: \n${handleError(e)}`);
+    }
+  };
+
+  return (
+    <BaseContainer>
+      <div className="auth container">
+        <div className="auth form">
+          <FormField
+            label="Email"
+            value={email}
+            onChange={(n) => setEmail(n)}
+          />
+          <FormField
+            label="Username"
+            value={username}
+            onChange={(uun) => setUsername(uun)}
+          />
+          <FormField
+            label="Password"
+            value={password}
+            onChange={(un) => setPassword(un)}
+          />
+          <div className="auth button-container">
+            <Button
+              disabled={!email || !password || !username}
+              onClick={() => doRegister()}
+            >
+              Register
+            </Button>
+          </div>
+          <div className="auth button-container">
+            <Button onClick={() => history.push("/login")}>
+              Sign in instead?
+            </Button>
+          </div>
+        </div>
+      </div>
+    </BaseContainer>
+  );
 };

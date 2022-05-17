84b894c4be8d330b994a5500415280eacf99ccd7
eoeaee <jerome.faessler98@gmail.com>
2022-05-17 16:45:50 +0200

    Adjusted burger menu styling

--- src/components/views/BurgerMenu.js
+++ src/components/views/BurgerMenu.js
@@ -9,13 +9,13 @@ class BurgerMenu extends React.Component {
         // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
         return (
             <Menu
-                width={ 500}
+                width={ "40vw"}
             >
-                <a className="menu-item" href="/user/profile">My Profile</a>
-                <a className="menu-item" href="/user/calendar">My Calendar</a>
-                <a className="menu-item" href="/team/profile">Team Profile</a>
-                <a className="menu-item" href="/team/calendar">Team Calendar</a>
-                <a className="menu-item" href="/game">Snake</a>
+                <a href="/user/profile">My Profile ➔</a>
+                <a href="/user/calendar">My Calendar ➔</a>
+                <a href="/team/profile">Team Profile ➔</a>
+                <a href="/team/calendar">Team Calendar ➔</a>
+                <a href="/game">Snake ➔</a>
             </Menu>
         );
     }

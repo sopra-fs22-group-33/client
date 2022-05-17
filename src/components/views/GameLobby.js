d3ba9f2f7391f28e29021085ed3e7276ae943b30
UpstairsForest <nikita.batalov@uzh.ch>
2022-05-17 10:06:21 +0200

    Implement rough game lobby
    (cherry picked from commit ffc68334c1e4ac953cf534a03e5abdc301e9cc11)

--- src/components/views/GameLobby.js
+++ src/components/views/GameLobby.js
@@ -1,7 +1,65 @@
 import * as React from "react";
+import { fetchGames } from "../../helpers/api";
+import { withRouter } from "react-router-dom";
+import { randomId } from "../../helpers/validations";
 
-export class GameLobby extends React.Component {
+class GameLobby extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      games: [],
+    };
+  }
+
+  componentDidMount() {
+    fetchGames(sessionStorage.getItem("id")).then((games) => {
+      this.setState({ games });
+    });
+  }
+
+  handleGameClick(ev, game, player) {
+    sessionStorage.setItem("gameId", game.id);
+    sessionStorage.setItem(
+      "playerId", player.id
+    );
+    this.props.history.push("/game");
+  }
+
+  getPlayerForUser(userId, players) {
+    let p, player;
+    for (p = 0; p < players.length; p++) {
+      player = players[p];
+      if (parseInt(sessionStorage.getItem("id")) === player.user.id) {
+        break;
+      }
+    }
+    return player;
+  }
+
   render() {
-    return null;
+    return (
+      <div>
+        {this.state.games.map((game) => {
+          const player = this.getPlayerForUser(
+            sessionStorage.getItem("id"),
+            game.players
+          );
+          return (
+            <ul
+              key={randomId()}
+              onClick={(ev) => this.handleGameClick(ev, game, player)}
+            >
+              <div>game id: {game.id}</div>
+              <div>my player id:{player.id}</div>
+              <div>my status: {player.status}</div>
+              <div>number of players: {game.players.length}</div>
+            </ul>
+          );
+        })}
+      </div>
+    );
   }
 }
+
+export default withRouter(GameLobby);

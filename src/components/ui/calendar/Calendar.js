47d31e9a68ca6c67ba741d4136126f10c7a3ec63
eoeaee <jerome.faessler98@gmail.com>
2022-05-19 14:56:12 +0200

    More and thinner lines on calendar

--- src/components/ui/calendar/Calendar.js
+++ src/components/ui/calendar/Calendar.js
@@ -45,9 +45,18 @@ export class Calendar extends React.Component {
           <div className={"time-label1"}>06:00</div>
           <div className={"time-label2"}>12:00</div>
           <div className={"time-label3"}>18:00</div>
+
+          <div className={"time-line1"}></div>
           <div className={"time-line2"}></div>
           <div className={"time-line3"}></div>
           <div className={"time-line4"}></div>
+          <div className={"time-line5"}></div>
+          <div className={"time-line6"}></div>
+          <div className={"time-line7"}></div>
+          <div className={"time-line8"}></div>
+          <div className={"time-line9"}></div>
+          <div className={"time-line10"}></div>
+          <div className={"time-line11"}></div>
         </Grid>
       </Box>
     );

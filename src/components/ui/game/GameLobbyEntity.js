ba32010351a8a0a2901c0fc4eb4ca8c1f7635b2a
UpstairsForest <nikita.batalov@uzh.ch>
2022-05-26 18:21:17 +0200

    Style game lobby entity

--- src/components/ui/game/GameLobbyEntity.js
+++ src/components/ui/game/GameLobbyEntity.js
@@ -2,6 +2,7 @@ import Box from "@mui/material/Box";
 import * as React from "react";
 import PropTypes from "prop-types";
 import { Player } from "../../../models/Player";
+import "styles/views/Team.scss";
 
 export const GameLobbyEntity = (props) => {
   function calcPlayersOnline(players) {
@@ -10,25 +11,24 @@ export const GameLobbyEntity = (props) => {
 
   return (
     <Box
+      className={"team container"}
       style={{
         padding: 20,
-        background: props.isSelected ? "lightcoral" : "lightgray",
+        background: props.isSelected ? "linear-gradient(to right, #3C4FFA, #3DD7F9)" : null,
       }}
       onClick={props.onClick}
     >
+      <div className={"team name 2"}>
+        players online: {calcPlayersOnline(props.game.players)} /{" "}
+        {props.game.players.length}
+      </div>
       <div>my status: {props.player ? props.player.statusOnline : null}</div>
-      <div>number of players: {props.game.players.length}</div>
       <div>
-        players:
         {props.game.players
           .sort((a, b) => (a.id < b.id ? 1 : -1))
           .map((player) => (
             <ul key={player.id}>{player.user.username}</ul>
           ))}
-      </div>
-      <div>
-        number of players online: {calcPlayersOnline(props.game.players)} /{" "}
-        {props.game.players.length}
       </div>
     </Box>
   );

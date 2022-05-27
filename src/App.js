d89ed6a5cd712b4bea36e22e26bb82dab91706e2
grueezi <mark.rueetschi@uzh.ch>
2022-05-27 09:17:19 +0200

    changed license

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import AppRouter from "components/routing/routers/AppRouter";
 
-/**
- * Happy coding!
- * React Template by Lucas Pelloni
- * Overhauled by Kyrill Hux
- */
+/*plan your teams working hours by using the shift planner
+Copyright (C) 2022 sopra-fs22-group-33
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.*/
+
 const App = () => {
   return (
       <AppRouter/>

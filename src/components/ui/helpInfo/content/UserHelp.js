3ba231fea810857f29e05e77cecb14a690bc6012
UpstairsForest <nikita.batalov@uzh.ch>
2022-05-28 17:41:00 +0200

    Add user preference help content

--- src/components/ui/helpInfo/content/UserHelp.js
+++ src/components/ui/helpInfo/content/UserHelp.js
@@ -1,5 +1,16 @@
+import editHelp from "../../../../images/helpScreenshots/userCalendarEdit.png"
+import {HelpInfoImageFrame} from "../HelpInfoImageFrame";
+
 export const UserCalendarEditHelp = (
-    "text or component"
+    <div>
+        <div>
+            Here you can set your preferences for different working hours on different weekdays
+        </div>
+        <HelpInfoImageFrame src={editHelp}/>
+        <div>
+            Once you are part of a team, these will be mapped onto the schedules in that team's calendar
+        </div>
+    </div>
 )
 
 export const UserCalendarHelp = (

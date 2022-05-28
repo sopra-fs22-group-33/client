d35da659c1bd3f25cb129cc3e95ae912e7ce8e30
UpstairsForest <nikita.batalov@uzh.ch>
2022-05-28 11:36:54 +0200

    Add button to leave game after death

--- src/components/ui/game/DeathDisplay.js
+++ src/components/ui/game/DeathDisplay.js
@@ -1,18 +1,27 @@
 import PropTypes from "prop-types";
 import * as React from "react";
+import { Button } from "../Button";
+import { useHistory } from "react-router-dom";
 
-export const DeathDisplay = (props) => (
-  <div
-    style={{
-      position: "relative",
-      top: props.length / 2,
-      background: "white",
-      fontSize: "20px",
-    }}
-  >
-    YOU DIED
-  </div>
-);
+export const DeathDisplay = (props) => {
+  const history = useHistory();
+  return (
+    <div
+      style={{
+        position: "relative",
+        top: props.length / 2,
+        background: "white",
+        fontSize: "20px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      YOU DIED
+      <Button onClick={() => history.push("/game/lobby")}>Leave</Button>
+    </div>
+  );
+};
 
 DeathDisplay.propTypes = {
   length: PropTypes.number.isRequired,

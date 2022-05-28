f63aad26d84c6a07e914f3bf019eff5e02409dd3
UpstairsForest <nikita.batalov@uzh.ch>
2022-05-28 17:36:25 +0200

    Add image styling component

--- src/components/ui/helpInfo/content/AuthHelp.js
+++ src/components/ui/helpInfo/content/AuthHelp.js
@@ -1,10 +1,12 @@
 import auth from "../../../../images/helpScreenshots/auth.png";
+import { HelpInfoImageFrame } from "../HelpInfoImageFrame";
 
 export const AuthHelp = (
   <div>
-      <div>
-          To start please create a new account or log into an existing one by providing your email and password
-      </div>
-    <img src={auth} style={{ width: "75%", height: "auto", }} alt={" "} />
+    <div>
+      To start please create a new account or log into an existing one by
+      providing your email and password
+    </div>
+    <HelpInfoImageFrame src={auth} />
   </div>
 );
